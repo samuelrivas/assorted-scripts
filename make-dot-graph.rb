178fa6fdfe60fffd1fc8ccc7fb05828a88369a9f
Samuel Rivas <samuelrivas@gmail.com>
2012-04-10 13:52:24 +0200

    FUNCT don't depend on ruby1.9
    It's not needed

--- make-dot-graph.rb
+++ make-dot-graph.rb
@@ -1,16 +1,15 @@
-#!/usr/bin/env ruby1.9
-
+#!/usr/bin/env ruby
 
 puts "digraph {"
 
 ARGF.each do |line|
 
   if (line =~ /(.*?)\s*=\s*(.*)$/)
-    puts ("#{$1}[label=\"#{$2}\"];")
+    puts("#{$1}[label=\"#{$2}\"];")
   elsif (line =~ /(.*?)\s*:\s*(.*)$/)
-    puts ("#{$1}[shape=box,label=\"#{$2}\"];")
+    puts("#{$1}[shape=box,label=\"#{$2}\"];")
   else
-    puts (line)
+    puts(line)
   end
 end
 
