86ab34e88bbfdc4a75a70f6f8db1e1e2a17b6f2b
Samuel Rivas <samuelrivas@gmail.com>
2012-05-06 16:40:05 +0200

    indent and avoid showing processes with 0 swap

--- get-swap.sh
+++ get-swap.sh
@@ -10,15 +10,17 @@
 SUM=0
 OVERALL=0
 for DIR in `find /proc/ -maxdepth 1 -type d | egrep "^/proc/[0-9]"` ; do
-PID=`echo $DIR | cut -d / -f 3`
-PROGNAME=`ps -p $PID -o comm --no-headers`
-for SWAP in `grep Swap $DIR/smaps 2>/dev/null| awk '{ print $2 }'`
-do
-let SUM=$SUM+$SWAP
-done
-echo "PID=$PID - Swap used: $SUM - ($PROGNAME )"
-let OVERALL=$OVERALL+$SUM
-SUM=0
+    PID=`echo $DIR | cut -d / -f 3`
+    PROGNAME=`ps -p $PID -o comm --no-headers`
+    for SWAP in `grep Swap $DIR/smaps 2>/dev/null| awk '{ print $2 }'`
+    do
+	let SUM=$SUM+$SWAP
+    done
+    if [ $SUM -gt 0 ]; then
+	echo "$SUM - PID=$PID - ($PROGNAME)"
+	let OVERALL=$OVERALL+$SUM
+    fi
+    SUM=0
 
 done
 echo "Overall swap used: $OVERALL"
