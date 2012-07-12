e9c0854635d952f3af7dadc01b9324100329bc16
Samuel Rivas <samuelrivas@gmail.com>
2012-07-12 22:29:33 +0200

    FUNCT script to add BSD licence to erlang files

---
+++ bsdise.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#
+# Adds a BSD header to an erlang file
+
+set -e
+
+SCRIPT_HOME="$( cd "$( dirname "$0" )" && pwd )"
+TEMPLATES="$SCRIPT_HOME/assorted-scripts/templates"
+DEST=$1
+
+if [ -z "$DEST" ]; then
+    echo
+    echo "usage: `basename $0` <file.hrl>"
+    echo
+    exit 1
+fi
+
+YEAR=`date +%Y`
+TEMP=/tmp/$DEST.bsdise
+cp $DEST /tmp/$DEST.bsdise
+sed -e "s/@@YEAR@@/$YEAR/g;s/@@COMM@@/%%%/g" \
+    < $TEMPLATES/bsd-licence.template \
+    > $DEST
+
+cat $TEMP >> $DEST
+
+echo
+echo "modified $DEST!"
+echo
