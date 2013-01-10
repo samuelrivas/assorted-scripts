721dc8e27e85d281148bec2693118e728dee38b9
Samuel Rivas <samuelrivas@gmail.com>
2013-01-10 10:02:09 +0100

    FUNCT detect the connected HDMI and LVDS output names
    Laptops tend to have a number of HDMI outputs, and the one that is wired is not
    always the same

--- screen-control.sh
+++ screen-control.sh
@@ -14,16 +14,32 @@ if [ -z "$COMMAND" ]; then
     exit 1
 fi
 
+setup_screen_names() {
+    XRANDR=$(xrandr)
+    HDMI=$(expr "$XRANDR" : '.*\(HDMI[0-9]*\) connected')
+    LVDS=$(expr "$XRANDR" : '.*\(LVDS[0-9]*\) connected')
+    echo "HDMI output: $HDMI"
+    echo "LVDS output: $LVDS"
+}
+
 case $COMMAND in
     turn-hdmi-off)
-        xrandr --output HDMI2 --off
+        setup_screen_names
+        xrandr --output $HDMI --off
         ;;
     turn-hdmi-on)
-        xrandr --output HDMI2 --auto --right-of LVDS1
+        setup_screen_names
+        xrandr --output $HDMI --auto --right-of $LVDS
         ;;
     turn-screen-off)
         xset dpms force off
         ;;
     set-brightness)
         /usr/bin/xbacklight -set $2
+        ;;
+    *)
+        echo
+        echo "$COMMAND is not a valid command"
+        echo
+        exit 1
 esac
