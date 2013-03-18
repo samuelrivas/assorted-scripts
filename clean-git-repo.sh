33ec42dc00ae4c932040ffd6d9dccdcd9ca32d80
Samuel Rivas <samuelrivas@gmail.com>
2013-03-18 12:19:58 +0100

    BUGFIX make clean-git-repo.sh work in the absence files to preserve
    The previous change introduced a bug for the special (but most likely) case when
    there are not files to preserve when resetting the repo

--- clean-git-repo.sh
+++ clean-git-repo.sh
@@ -10,7 +10,7 @@ SAVE_FILES_FILE="$HOME/.clean-git-repo.save-files"
 if [ -f $SAVE_FILES_FILE ]; then
     TEMP=$(mktemp -d)
     for i in $(cat $SAVE_FILES_FILE); do
-        [ -f $i ] && echo "** saving    $i"; mv $i $TEMP
+        [ -f $i ] && (echo "** saving    $i"; mv $i $TEMP)
     done
 fi
 
@@ -19,7 +19,7 @@ git submodule foreach --recursive git clean -dfx
 
 if [ -f $SAVE_FILES_FILE ]; then
     for i in $(cat $SAVE_FILES_FILE); do
-        [ -f $TEMP/$i ] && echo "** restoring $i"; cp $TEMP/$i $i
+        [ -f $TEMP/$i ] && (echo "** restoring $i"; cp $TEMP/$i $i)
     done
     rm -rf $TEMP
 fi
