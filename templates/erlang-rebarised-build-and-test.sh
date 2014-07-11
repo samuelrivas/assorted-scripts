7d2bd8a05a7fde374b0e90d1891742221f6c0841
Samuel Rivas <samuelrivas@gmail.com>
2014-07-11 11:19:59 +0200

    FUNCT better build_and_test.sh script

--- templates/erlang-rebarised-build-and-test.sh
+++ templates/erlang-rebarised-build-and-test.sh
@@ -1,29 +1,7 @@
 #!/bin/sh
-# This is the script run by jenkins to verify this module
+# The CI server should run this script to verify any version
 
 set -e
-
-echo "============================================================"
-echo " Cleaning previous artifacts"
-echo "============================================================"
 git clean -dfx
 
-echo "============================================================"
-echo " Compling datetime"
-echo "============================================================"
-make compile
-
-echo "============================================================"
-echo " Creating the documentation to check it builds correctly"
-echo "============================================================"
-make docs
-
-echo "============================================================"
-echo " Static checks for datetime"
-echo "============================================================"
-make check
-
-echo "============================================================"
-echo " Unit testing datetime"
-echo "============================================================"
-make test
+make -k check docs test
