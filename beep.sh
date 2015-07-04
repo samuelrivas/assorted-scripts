517b9521aaac98a1b9527888c0f919357c42e0dc
Samuel Rivas <samuelrivas@gmail.com>
2015-07-04 20:44:51 +0200

    Find echo instead of assuming it is in /bin
    That makes this more portable, specifically for nixos
    
    Also add a comment to clarify why we don't use the shell builtin echo.

--- beep.sh
+++ beep.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-/bin/echo -e '\a'
+# Bash's and dash's builtin echos interpret flags differently, so we use the
+# actual binary hoping for more consistent behaviour across systems (since some
+# systems cheat and symlink /bin/sh to bash whereas others are a bit more
+# civilised and symlink it to dash
+"$(which echo)" -n -e '\a'
