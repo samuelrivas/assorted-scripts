86f49c646e34d055760396cdef482bffca970a2a
Samuel Rivas <samuel.rivas@schibsted.com>
2015-10-15 10:10:13 +0200

    Make this depend on our source, not on github's source

--- nix/assorted-scripts.nix
+++ nix/assorted-scripts.nix
@@ -1,14 +1,9 @@
-{ stdenv, fetchFromGitHub, xbacklight, xrandr, xset }:
+{ stdenv, xbacklight, xrandr, xset }:
 
 stdenv.mkDerivation rec {
-  name = "assorted-scripts-0.0.2";
+  name = "assorted-scripts-dev";
 
-  src = fetchFromGitHub {
-    owner = "samuelrivas";
-    repo = "assorted-scripts";
-    rev = "517b9521aaac98a1b9527888c0f919357c42e0dc";
-    sha256 = "1l3jyqgddd0lm0macxap8sz83hjzqwi1frynlikd3gxlafrrvidy";
-  };
+  src = ./..;
 
   inherit xbacklight xrandr xset;
 
